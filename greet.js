8c4c87ea8b389aff6aad37344c589e4cbecb5f65
Tricia Buckenberger <tbuckenberger@gmail.com>
2015-02-03 13:38:57 -0800

    extra credit test

--- greet.js
+++ greet.js
@@ -1,5 +1,12 @@
 'use strict';
 
 var greeting = require('./simple');
+var hello = exports = module.exports = {};
 
-console.log(greeting.greet(process.argv[2]));
+hello.person = function() {
+  var result = greeting.greet(process.argv[2]);
+  console.log(result);
+  return result;
+};
+
+hello.person();
